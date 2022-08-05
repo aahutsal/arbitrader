91e7893d7b537954577cfeaaa6be05ecca2fcc05
Arsen A. Hutsal <aahutsal@gmail.com>
2022-08-05 10:07:56 +0300

    ccxt and .env.sample initialization added

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,21 @@
-import { Swapper, Networks, getToken, getSwapTransaction, createSwapper } from '../lib/swapper'
-import { ParaSwap, NetworkID, Token, APIError, SwapSide } from 'paraswap'
-import { BigNumber } from 'bignumber.js'
+import ccxt, { Exchange } from 'ccxt'
+import dotenv from 'dotenv'
+import { getSwapTransaction, Networks } from '../lib/swapper'
+dotenv.config({ path: '../.env' })
+console.log(process.env.OKX_API_KEY)
 
+
+let kucoin: Exchange, okx: Exchange
+
+let initExchanges = () => {
+    [kucoin, okx] = [new ccxt.kucoin({
+        apiKey: process.env.KUCOIN_API_KEY,
+        secret: process.env.KUCOIN_SECRET
+    }), new ccxt.okx({
+        apiKey: process.env.OKX_API_KEY,
+        secret: process.env.OKX_SECRET
+    })]
+}
 export async function app() {
     const USER_ADDRESS = '0xD2236a1ccd4ced06E16eb1585C8c474969A6CcfE'
     const srcToken = 'BNB'
